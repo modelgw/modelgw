9d3780a025085863f1c84215e94faa083ac0c1a5
Igor Urmincek <igor.urmincek@gmail.com>
2024-06-05 21:47:00 +0200

    Updated error handling and logs

--- src/endpoints/gateway/handler.ts
+++ src/endpoints/gateway/handler.ts
@@ -3,9 +3,10 @@ import { GatewayKeyConst, InferenceEndpointConst } from '@/lib/db/const';
 import { logger as mainLogger } from "@/lib/logger";
 import { createId } from '@paralleldrive/cuid2';
 import { InferenceEndpoint } from '@prisma/client';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { createHash } from "crypto";
 import { Request, RequestHandler, Response } from 'express';
+import lodash from 'lodash';
 import pino from 'pino';
 import { GatewayExhaustedResponse, GatewayExhaustedResponseStatus, OpenAIUnauthorizedResponse, OpenAIUnauthorizedResponseStatus } from './response';
 import { createHeadersFromRequest, resetThrottlingEndpoints, setEndpointStatus, shuffleArray } from './utils';
@@ -59,9 +60,9 @@ export const createGatewayHandler = ({ prismaClient }: CreateGatewayHandlerOptio
 		});
 
 		let inferenceEndpoints = gateway.inferenceEndpoints.map(value => value.inferenceEndpoint);
-		logger.info({ inferenceEndpoints }, 'Inference endpoints');
+		logger.info({ inferenceEndpoint: inferenceEndpoints.map(e => ({ id: e.id })) }, 'Inference endpoints');
 		shuffleArray(inferenceEndpoints);
-		logger.info({ inferenceEndpoints }, 'Shuffled inference endpoints');
+		logger.info({ inferenceEndpoint: inferenceEndpoints.map(e => ({ id: e.id })) }, 'Shuffled inference endpoints');
 
 		for (const endpoint of inferenceEndpoints) {
 			const processed = await requestEndpoint({ prismaClient, req, res, endpoint, logger });
@@ -78,17 +79,22 @@ type RequestAzureOpenAIOptions = {
 	endpoint: InferenceEndpoint;
 	logger: pino.Logger;
 };
-export async function requestEndpoint({ req, res, endpoint, prismaClient, logger }: RequestAzureOpenAIOptions) {
+export async function requestEndpoint({ req, res, endpoint, prismaClient, logger: mainLogger }: RequestAzureOpenAIOptions) {
+	const logger = mainLogger.child({ inferenceEndpoint: { id: endpoint.id } });
 	const headers = createHeadersFromRequest(req);
-	const body = { ...req.body }; //shallow copy
+	const body = req.is('json') ? { ...req.body } : req.body; //shallow copy
 	let url = new URL(req.url, endpoint.endpoint).toString();
 	if (endpoint.platform === InferenceEndpointConst.Platform.AzureOpenAI) {
 		headers['api-key'] = endpoint.key!;
-		if (body.model === 'auto') body.model = endpoint.deploymentName;
+		if (body.model === 'auto') {
+			body.model = endpoint.deploymentName;
+		}
 		url = url.replace('/auto/', `/${endpoint.deploymentName}/`);
+	} else if (endpoint.key) {
+		headers['authorization'] = `Bearer ${endpoint.key}`;
 	}
 
-	logger.debug({ url, body, endpoint: { id: endpoint.id } }, 'Requesting endpoint');
+	logger.debug({ url, body }, 'Sending request to inference endpoint');
 	const startTime = performance.now();
 	try {
 		var fetchResponse = await axios({
@@ -96,28 +102,37 @@ export async function requestEndpoint({ req, res, endpoint, prismaClient, logger
 			method: req.method,
 			headers,
 			responseType: 'stream',
-			data: body,
+			data: req.method == 'GET' || lodash.isEmpty(body) ? undefined : body,
 		});
-	} catch (error) {
-		logger.error({ error }, 'Azure OpenAI request failed');
-		await setEndpointStatus(endpoint.id, InferenceEndpointConst.Status.Error, 30, 'Unavailable', prismaClient);
+	} catch (error: unknown | AxiosError) {
+		const endTime = performance.now();
+		logger.error({ error, duration: endTime - startTime }, 'Inference endpoint request failed');
+		if (axios.isAxiosError(error)) {
+			// Access to config, request, and response
+			if (error.response) {
+				// The request was made and the server responded with a status code that falls out of the range of 2xx
+				const retryAfterSec = error.response.headers['retry-after'] ? parseInt(error.response.headers['retry-after']) : undefined;
+				if (error.response.status == 429) {
+					await setEndpointStatus(endpoint.id, InferenceEndpointConst.Status.Throttling, retryAfterSec, null, prismaClient);
+				} else if (error.response.status == 401 || error.response.status == 403) {
+					await setEndpointStatus(endpoint.id, InferenceEndpointConst.Status.Error, retryAfterSec, error.response.statusText, prismaClient);
+				}
+			} else if (error.request) {
+				// The request was made but no response was received
+				await setEndpointStatus(endpoint.id, InferenceEndpointConst.Status.Error, 30, error.message, prismaClient);
+			} else {
+				// Something happened in setting up the request that triggered an Error
+				await setEndpointStatus(endpoint.id, InferenceEndpointConst.Status.Error, 30, error.message, prismaClient);
+			}
+		} else {
+			await setEndpointStatus(endpoint.id, InferenceEndpointConst.Status.Error, 30, String(error), prismaClient);
+		}
 		return false;
 	}
+	// got 2xx response
 	const endTime = performance.now();
-	logger.info({ headers: fetchResponse.headers, body, duration: endTime - startTime }, 'Azure OpenAI request completed');
-	if (fetchResponse.status == 200) {
-		res.header({ ...fetchResponse.headers, 'x-gateway-endpoint-id': endpoint.id.toString() });
-		fetchResponse.data.pipe(res);
-		// fetchResponse.body!.pipe(res);
-		return true;
-	} else if (fetchResponse.status == 429) {
-		const retryAfterSec = parseInt(fetchResponse.headers['retry-after'] ?? '10');
-		await setEndpointStatus(endpoint.id, InferenceEndpointConst.Status.Throttling, retryAfterSec, null, prismaClient);
-	} else if (fetchResponse.status == 401) {
-		await setEndpointStatus(endpoint.id, InferenceEndpointConst.Status.Error, null, 'Unauthorized', prismaClient);
-	} else if (fetchResponse.status == 403) {
-		await setEndpointStatus(endpoint.id, InferenceEndpointConst.Status.Error, null, 'Forbidden', prismaClient);
-	}
-	await setEndpointStatus(endpoint.id, InferenceEndpointConst.Status.Error, 30, 'Unavailable', prismaClient);
-	return false;
+	logger.info({ headers: fetchResponse.headers, body, duration: endTime - startTime }, 'Inference endpoint request completed');
+	res.header({ ...fetchResponse.headers, 'x-modelgw-inference-endpoint-id': endpoint.id.toString() });
+	fetchResponse.data.pipe(res);
+	return true;
 }
