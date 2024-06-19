82bc85ef5044ce9a3d29ad3b282023a8553c97d1
Igor Urmincek <igor.urmincek@gmail.com>
2024-06-19 14:00:10 +0200

    Improved async error handling

--- src/endpoints/gateway/gateway-server.ts
+++ src/endpoints/gateway/gateway-server.ts
@@ -1,7 +1,23 @@
-import { logger } from "@/lib/logger";
-import express from "express";
+import { logger } from '@/lib/logger';
+import express, { NextFunction, Request, Response } from 'express';
 import requestIp from 'request-ip';
-import { createGatewayHandler } from "./handler";
+import { createGatewayHandler } from './handler';
+
+function asyncErrorBoundary(func: (req: Request, res: Response, next: NextFunction) => void) {
+  return async function (req: Request, res: Response, next: NextFunction) {
+    try {
+      await func(req, res, next);
+    }
+    catch (err) {
+      next(err);
+    }
+  }
+}
+
+function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+  logger.error({ err, url: req.url, method: req.method }, 'Error in gateway endpoint');
+  res.status(500).json({ error: 'Internal server error' });
+}
 
 export function createGatewayServer(prismaClient: any, GATEWAY_PORT: number) {
 
@@ -9,7 +25,8 @@ export function createGatewayServer(prismaClient: any, GATEWAY_PORT: number) {
   app.disable('x-powered-by');
   app.use(requestIp.mw());
   app.use(express.json());
-  app.all('/*', createGatewayHandler({ prismaClient }));
+  app.all('/*', asyncErrorBoundary(createGatewayHandler({ prismaClient })));
+  app.use(errorHandler);
 
   const server = app.listen(GATEWAY_PORT, function () {
     logger.info({ port: GATEWAY_PORT }, 'Gateway listening');
